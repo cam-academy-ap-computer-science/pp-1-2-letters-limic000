bece734e60e5df775e6bc5c6e7cfa11831dabb03
li_mic000 <li_mic000@CAM-LAB-20.bgsd.k12.wa.us>
2019-09-16 07:50:01 -0700

    Finished at school!

--- src/Letters.java
+++ src/Letters.java
@@ -2,8 +2,42 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GANSON();
+		System.out.println("");
+		Emmy();
+		System.out.println("");
+		Vii();
 	}
-
+	
+	public static void greetings() {
+		System.out.print("Hi, how are you ");
+	}
+	public static void common() {
+		System.out.println("Okay I gotta go do homework now, baiii");
+		System.out.println("Michelle :D");
+	}
+	public static void GANSON() {
+		greetings();
+		System.out.println("GANSON?");
+		System.out.println("Are you enjoying junior year lately?");
+		System.out.println("I hope you are doing well in your classes!");
+		common();
+	}
+	
+	public static void Emmy() {
+		greetings();
+		System.out.println("Emmy?");
+		System.out.println("How is your mural project going along?");
+		System.out.println("I'm almost done with my tile :)");
+		common();
+	}
+	
+	public static void Vii() {
+		greetings();
+		System.out.println("Vii?");
+		System.out.println("Did you get enough sleep last night?");
+		System.out.println("I sure hope you did because that makes at least one of us");
+		System.out.println("I'm so tired haha");
+		common();
+	}
 }
